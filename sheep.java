5832a861e10fec0d273bbee611cdff1f8b0e93c3
whs <asfdff>
2018-09-27 08:37:48 +0800

    111

---
+++ sheep.java
@@ -0,0 +1,9 @@
+class Sheep extends Animal{
+    String name = "Ian";
+    int age = 3;
+    Sheep(){};
+    Sheep(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+}
